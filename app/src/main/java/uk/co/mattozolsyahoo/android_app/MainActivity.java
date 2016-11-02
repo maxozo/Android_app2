2eaca768e4ded43dd0876af26d0758beb97c86be
Matiss Ozols <mattozols@yahoo.co.uk>
2016-11-02 10:30:23 +0000

    Initial commit

---
+++ app/src/main/java/uk/co/mattozolsyahoo/android_app/MainActivity.java
@@ -0,0 +1,20 @@
+package uk.co.mattozolsyahoo.android_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void TestUpload(){
+
+
+        int i = i+1;
+
+    }
+}
